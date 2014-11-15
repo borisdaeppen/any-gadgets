0e2cd4d608394802e65aecf6252d3a2311910b26
Boris Däppen <boris_daeppen@bluewin.ch>
2014-11-15 23:22:25 +0100

    -12 chars

--- kreis_oneliner.sh
+++ kreis_oneliner.sh
@@ -1,1 +1,1 @@
-perl -E '$r=20;for$x(1..$r*2){for$y(1..$r*2){if(($r-$x)**2+($r-$y)**2<$r**2){print"."}else{print" "}}say}'
+perl -E '$r=20;for$x(1..$r*2){for$y(1..$r*2){print(($r-$x)**2+($r-$y)**2<$r**2 ?".":" ")}say}'
