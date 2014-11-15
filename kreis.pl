e09f9a24c91e33702a622460e606ee647bd9f090
Boris Däppen <boris_daeppen@bluewin.ch>
2014-11-15 22:18:16 +0100

    comments and bug fix in size

--- kreis.pl
+++ kreis.pl
@@ -1,23 +1,33 @@
+# draw an ascii circle
 use strict;
 use warnings;
 
+# radius
 my $r = $ARGV[0] || 15;
 
+# char for drawing
+my $c = $ARGV[1] || '.';
+
+# rows are diameter (2 x radius)
 for (my $x=1; $x<=$r*2; $x++) {
+    # columns are also diameter
     for (my $y=1; $y<=$r*2; $y++) {
+
+        # if Phytagoras tells us point is in radius...
         if (
                 (($r - $x)*($r - $x))
               + (($r - $y)*($r - $y))
             
-          < $r * $r / 2) {
-            print "#";
+          < $r * $r) {
+
+            # ...print a char...
+            print $c;
         }
         else {
-            print " ";
+            # ...otherwise a space
+            print ' ';
         }
     }
+    # start next row
     print "\n";
 }
-
-#print "\n";
-
