acb5978f2c9de656f07c9b32810645a6bba1ca63
Boris Däppen <boris.daeppen@gibb.ch>
2016-12-09 14:26:11 +0100

    initial try to read and write __DATA__

---
+++ perl/data_section/read.pl
@@ -0,0 +1,9 @@
+
+foreach (<DATA>) {
+    print("- $_");
+}
+
+__END__
+1
+2
+The End of The End
---
+++ perl/data_section/write.pl
@@ -0,0 +1,8 @@
+use feature say;
+
+say STDERR 'Some Error';
+say STDOUT 'Some Message';
+say DATA   'Some Data';
+
+__DATA__
+foo bar
